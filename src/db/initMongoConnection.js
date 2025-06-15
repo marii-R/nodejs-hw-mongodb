1280e4b8018d78389856a07e56f270a10517ca41
Marii R <mariia.revak@gmail.com>
2025-06-15 23:47:24 +0300

    fix

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,13 +1,15 @@
 import mongoose from 'mongoose';
 
 export async function initMongoConnection() {
-  const { MONGODB_URL } = process.env;
+  const { MONGODB_USER, MONGODB_PASSWORD, MONGODB_URL, MONGODB_DB } = process.env;
+
+  const mongoURI = `mongodb+srv://${MONGODB_USER}:${MONGODB_PASSWORD}@${MONGODB_URL}/${MONGODB_DB}?retryWrites=true&w=majority`;
 
   try {
-    await mongoose.connect(MONGODB_URL);
+    await mongoose.connect(mongoURI);
     console.log('Mongo connection successfully established!');
   } catch (err) {
-    console.error('Mongo connection failed:', err);
+    console.error('Mongo connection failed:', err.message);
     process.exit(1);
   }
 }
